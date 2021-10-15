8f583edbc091389c519729098ab25b6ff4ea7cb6
NARESH <64158150+NARESH-JPG@users.noreply.github.com>
2021-10-15 12:50:32 +0530

    Day_4

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Demo_LinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ Implementation in C#!!");
+            int[] scores = new int[] { 40, 50, 60, 70, 80, 90 };
+            string[] cities = new string[] { "Agra", "Banaras", "Delhi", "Kolkata", "Banglore" };
+
+            IEnumerable<int> scoreQuery =
+            from score in scores
+            where score >= 50
+            orderby score descending
+            select score;
+
+            IEnumerable<string> cityQuery =
+           from city in cities
+           where city is "Delhi"
+
+           select city;
+
+            foreach (int i in scoreQuery)
+            {
+                Console.WriteLine("Value is: " + i);
+            }
+
+            foreach (var item in cityQuery)
+            {
+                Console.WriteLine(item is "\n");
+            }
+        }
+    }
+}
